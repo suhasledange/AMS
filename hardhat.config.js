699c749e9404a6559af6deac6b0de42f32769b02
suhasledange <suhasledange10@gmail.com>
2024-03-30 11:06:08 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,14 @@
+const { conf } = require("./util/conf");
+
 require("@nomicfoundation/hardhat-toolbox");
 
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.18",
-// };
-const PRIVATE_KEY = "ec7c026af1128373e9ed6ab98189792a3f07e043d3435029fce99d9beb171bcc"
-const URL="https://sepolia.infura.io/v3/047abb4be9a143bd8fb3dc8142d2fedf"
+
 module.exports = {
 
    networks:{
       sepolia: {
-        url: URL,
-        accounts: [PRIVATE_KEY],
+        url: conf.URL,
+        accounts: [conf.PRIVATE_KEY],
       },
    },
 
---
+++ util/conf.js
@@ -0,0 +1,4 @@
+export const conf = {
+    URL:String(process.env.NEXT_PUBLIC_URL),
+    PRIVATE_KEY:String(process.env.NEXT_PUBLIC_PRIVATE_KEY),
+}
