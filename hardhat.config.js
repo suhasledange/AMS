e9d241e00149eb99e59a977fa1e0bfd3bdb682fd
Suhas Ledange <103985325+suhasledange@users.noreply.github.com>
2024-04-08 21:26:31 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,5 @@
 
 require("@nomicfoundation/hardhat-toolbox");
-
-const PRIVATE_KEY = "ec7c026af1128373e9ed6ab98189792a3f07e043d3435029fce99d9beb171bcc"
-const URL="https://arbitrum-sepolia.infura.io/v3/047abb4be9a143bd8fb3dc8142d2fedf"
 module.exports = {
 
    networks:{
